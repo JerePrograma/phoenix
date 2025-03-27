45376fb1a96a60c08b9edd9d5d6d8d74d9d2e3f4
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2025-03-27 10:25:32 -0300

    Configuracion para despliegue en VPS

--- preneur_challenge/test/support/data_case.ex
+++ preneur_challenge/test/support/data_case.ex
@@ -32,22 +32,18 @@ defmodule PreneurChallenge.DataCase do
     :ok
   end
 
-
-  Sets up the sandbox based on the test tags.
-  """
+  # Sets up the sandbox based on the test tags.
   def setup_sandbox(tags) do
     pid = Ecto.Adapters.SQL.Sandbox.start_owner!(PreneurChallenge.Repo, shared: not tags[:async])
     on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
   end
 
+  # A helper that transforms changeset errors into a map of messages.
 
-  A helper that transforms changeset errors into a map of messages.
+  assert {:error, changeset} = Accounts.create_user(%{password: "short"})
+  assert "password is too short" in errors_on(changeset).password
+  assert %{password: ["password is too short"]} = errors_on(changeset)
 
-      assert {:error, changeset} = Accounts.create_user(%{password: "short"})
-      assert "password is too short" in errors_on(changeset).password
-      assert %{password: ["password is too short"]} = errors_on(changeset)
-
-  """
   def errors_on(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _, key ->
